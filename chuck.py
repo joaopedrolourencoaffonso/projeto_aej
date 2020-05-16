7c12c2aafa394b5d13de6b37d08b092890a6f676
joaopedrolourencoaffonso <56265673+joaopedrolourencoaffonso@users.noreply.github.com>
2020-05-16 14:06:23 -0300

    Add files via upload

---
+++ chuck.py
@@ -0,0 +1,20 @@
+import sys, webbrowser, requests, json, subprocess
+
+def main():
+        for num in range(1,100):
+                res = requests.get("http://api.icndb.com/jokes/random/")
+                res.raise_for_status()
+                obj = res.json()
+                print(obj["value"]["joke"])
+                joke = obj["value"]["joke"]
+                string = '{ "joke":"' + str(joke) + '" }'
+                file = open("chuck.json", "w")
+                file.write(string)
+                file.close()
+                numero = str(num)
+                subprocess.call('curl -H "Content-Type: application/json" --data @chuck.json http://localhost:9200/jokes/_doc/' + numero + '?pretty')
+        
+
+if __name__ == '__main__':
+    main()
+
