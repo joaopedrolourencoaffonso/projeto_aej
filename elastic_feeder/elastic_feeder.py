1a03f2ac033330ab9cb644305368e7c09fc20f92
joaopedrolourencoaffonso <56265673+joaopedrolourencoaffonso@users.noreply.github.com>
2020-05-25 19:00:40 -0300

    Update elastic_feeder.py

--- elastic_feeder/elastic_feeder.py
+++ elastic_feeder/elastic_feeder.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-import subprocess, json
+import subprocess, json, requests
 
 app = Flask(__name__)
 
@@ -25,7 +25,34 @@ def add(word):
                 return content
         except:
                 return "Error"
-        
+
+@app.route('/exclude/<string:word>', methods=['GET', 'POST'])
+def exclude(word):
+        try:
+                temp = word.split('|')
+                index = temp[0]
+                _id = temp[1]
+                subprocess.call('curl -XDELETE http://localhost:9200/' + index +'/_doc/' + str(_id) + '?pretty')
+                return "Sucess!"
+                
+        except:
+                return "Error"
+
+
+@app.route('/show/<string:word>', methods=['GET'])
+def show(word):
+        try:
+                temp = word.split('|')
+                index = temp[0]
+                _id = temp[1]
+                res = requests.get("http://localhost:9200/" + str(index) + "/_doc/" + str(_id))
+                res.raise_for_status()
+                obj = res.json()
+                retorno = "Nome: " + str(obj["_source"]["nome"]) + " | Cor Favorita: " + str(obj["_source"]["favorite_color"])
+                return retorno
+
+        except:
+                return "Error"
 
 if __name__ == '__main__':
         app.run(debug=True)
