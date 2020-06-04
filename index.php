926eed85b999b10f5a804a4a1b31a96eab1eacd7
joaopedrolourencoaffonso <56265673+joaopedrolourencoaffonso@users.noreply.github.com>
2020-06-04 18:10:23 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Database</title>
+<style>
+	div.corpo {
+	  text-align: center;
+	}
+	a:link, a:visited {
+	  background-color: #f44336;
+	  color: white;
+	  padding: 15px 25px;
+	  text-align: center;
+	  text-decoration: none;
+	  display: inline-block;
+	}
+
+	a:hover, a:active {
+	  background-color: red;
+	}
+</style>
+</head>
+
+<body>
+
+<div class="corpo">
+	<h2>Bem vindo!</h2>
+	<p>Esse é um site feito em flask!</p>
+	<p>Para verificar os elementos a partir da id basta usar o path /search_id/{numero_inteiro_aqui}</p>
+	<!--<a href="script.php" target="_blank">Relatórios Gerenciais</a>-->
+	<br>
+
+</div>
+
+</body>
+</html>
