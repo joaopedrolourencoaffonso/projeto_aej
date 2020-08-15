fcaf5a69ab74d7556dc92990a9fb0c01e9cc346b
joaopedrolourencoaffonso <56265673+joaopedrolourencoaffonso@users.noreply.github.com>
2020-08-15 15:58:19 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>matemática!</title>
+
+
+</head>
+
+<body>
+
+	<p>Barcos!</p>
+	<div id="nome"></div>
+	<img src="../static/img/teste.jpg" alt="some text" width=240 height=160>
+	<script type="text/javascript" src="{{ url_for('static', filename='endswith2.js') }}"></script>
+
+</body>
+</html>
